7be9d949947b56e82ce8edf74510c9b6c367747f
mmoya26 <mmoya18@icloud.com>
2024-05-20 00:30:00 -0500

    Created and finish HTML and CSS for percentage-overview component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { ExpenseFormComponent } from './expense-form/expense-form.component';
 import { MonthlyStatsComponent } from './monthly-stats/monthly-stats.component';
+import { PercentageOverviewComponent } from './percentage-overview/percentage-overview.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, ExpenseFormComponent, MonthlyStatsComponent],
+  imports: [RouterOutlet, ButtonModule, ExpenseFormComponent, MonthlyStatsComponent, PercentageOverviewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/percentage-overview/percentage-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'percentage-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './percentage-overview.component.html',
+  styleUrl: './percentage-overview.component.css'
+})
+export class PercentageOverviewComponent {
+
+}
