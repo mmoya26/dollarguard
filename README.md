3181457bcd626ce61f7a74c87ee94b9349ee9912
mmoya26 <mmoya18@icloud.com>
2024-05-25 19:45:54 -0500

    Added validation checks for expense form + update readme

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ Personal project to learn Angular and everything involded with it.
 ## Todo: Expense Form (not high priority)
 - Add character count to notes text input field
 - Remove outline from date field when clicking away
-- Add pKeyFilter directive to amount field
-
 
 ## Todo in general
 - Figure out how the month selection functionality is going to look like
+- Fix gap issue in the calendar title (month and year are glued together)
+- Fix bug where if user leaves category option clicked error message displays
--- src/app/expense-form/expense-form.component.ts
+++ src/app/expense-form/expense-form.component.ts
@@ -6,11 +6,13 @@ import { InputTextModule } from 'primeng/inputtext';
 import { DropdownModule } from 'primeng/dropdown';
 import { CalendarModule } from 'primeng/calendar';
 import { KeyFilterModule } from 'primeng/keyfilter';
+import {FormBuilder,  FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import { CommonModule, formatDate } from '@angular/common';
 
 @Component({
   selector: 'expense-form',
   standalone: true,
-  imports: [KeyFilterModule, FloatLabelModule, InputGroupModule, InputGroupAddonModule, InputTextModule, DropdownModule, CalendarModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, KeyFilterModule, FloatLabelModule, InputGroupModule, InputGroupAddonModule, InputTextModule, DropdownModule, CalendarModule],
   templateUrl: './expense-form.component.html',
   styleUrl: './expense-form.component.css'
 })
@@ -19,7 +21,38 @@ export class ExpenseFormComponent {
   
   blockSpaceAndOnlyAllowNumbers : RegExp = /^\d*\.?\d*$/;
 
-  handleSubmit() {
-    alert("Submitted!!!!");
+  expenseForm = this.formBuilder.group({
+    category: ['', Validators.required],
+    amount: ['',  Validators.required],
+    date: ['',  Validators.required],
+    note: ['']
+  })
+
+  get category() {
+    return this.expenseForm.get('category');
   }
+
+  get amount() {
+    return this.expenseForm.get('amount');
+  }
+
+  get date() {
+    return this.expenseForm.get('date');
+  }
+
+  onSubmit() {
+    if (!this.expenseForm.invalid) {
+      console.log("form is valid!!");
+    } else {
+      console.log("Form is invalid");
+      this.expenseForm.markAllAsTouched();
+    }
+  }
+
+  // Format date to not include names and only numbers mm/dd/yyyy before setting Date Form control
+  onDateSelect(date : Date) {
+    this.expenseForm.controls['date'].setValue(date.toLocaleDateString([], { day: '2-digit', month: '2-digit', year: 'numeric', }));
+  }
+
+  constructor(private formBuilder: FormBuilder) {}
 }
