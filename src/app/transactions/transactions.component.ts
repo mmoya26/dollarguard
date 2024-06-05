1b49ac06d0da85cc58ef4e2aaa366e7da9b96197
mmoya26 <mmoya18@icloud.com>
2024-06-04 20:40:52 -0500

    Update transactions table to use DatePipe for displaying transaction dates

--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -3,11 +3,12 @@ import { Transaction } from '../interfaces/transaction';
 import { CategoryService } from '../services/category.service';
 import { Subscription } from 'rxjs/internal/Subscription';
 import { TransactionsService } from '../services/transactions.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'transactions',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './transactions.component.html',
   styleUrl: './transactions.component.css'
 })
