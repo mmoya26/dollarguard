a7010d4aba75b798c96bf8f634a6a415a488fa88
mmoya26 <mmoya18@icloud.com>
2024-08-08 22:09:55 -0500

    Initial work for NestJS authentication

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // Call first to have access to .env variables
+  dotenv.config();
 
+  const app = await NestFactory.create(AppModule);
+    
   app.enableCors({
     origin: 'http://localhost:4200',  // Allow only Angular app to access
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
