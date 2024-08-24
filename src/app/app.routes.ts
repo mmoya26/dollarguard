5e61b3754231fa0b6cd89524fd415ff4aaa6b7be
mmoya26 <mmoya18@icloud.com>
2024-08-24 14:13:03 -0500

    Temporarily remove guards from year selection page and expenses page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,8 @@ import { SignUpComponent } from './pages/auth/signup/signup.component';
 import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-    {path: 'expenses/:year/:month', component: ExpensesComponent, canActivate: [AuthGuard]},
-    {path: 'expenses', component: YearsSelectionComponent, canActivate: [AuthGuard]},
+    {path: 'expenses/:year/:month', component: ExpensesComponent},
+    {path: 'expenses', component: YearsSelectionComponent},
     {path: 'login', component: LoginComponent},
     {path: 'signup', component: SignUpComponent},
     {path: '', redirectTo: '/login', pathMatch: 'full'}
