6057112f56124cd4d50edbc46352229c1f38ada9
mmoya26 <mmoya18@icloud.com>
2024-10-13 14:01:35 -0500

    Create layout for auth pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,25 @@ import { ExpensesComponent } from './pages/expenses/expenses.component';
 import { YearsSelectionComponent } from './pages/years-selection/years-selection.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { SignUpComponent } from './pages/auth/signup/signup.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {path: 'expenses/:year/:month', component: ExpensesComponent, canActivate: [AuthGuard]},
     {path: 'expenses', component: YearsSelectionComponent, canActivate: [AuthGuard]},
-    {path: 'login', component: LoginComponent},
-    {path: 'signup', component: SignUpComponent},
-    {path: '', redirectTo: '/login', pathMatch: 'full'}
+    {path: '', component: AuthLayoutComponent, children: [
+        {
+            path: '',
+            redirectTo: 'login',
+            pathMatch: 'full',
+        },
+        {
+            path: 'login',
+            component: LoginComponent
+        },
+        {
+            path: 'signup',
+            component: SignUpComponent
+        }
+    ]}
 ];
---
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-auth-layout',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './auth-layout.component.html',
+  styleUrl: './auth-layout.component.css'
+})
+export class AuthLayoutComponent {
+
+}
