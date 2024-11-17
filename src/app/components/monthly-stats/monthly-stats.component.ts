8319241b1a6caed9463aa0ad1a1aa1b17a01dc9b
mmoya26 <mmoya18@icloud.com>
2024-11-17 00:24:27 -0600

    Add highest amount monthly stats card

--- src/app/components/monthly-stats/monthly-stats.component.ts
+++ src/app/components/monthly-stats/monthly-stats.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ExpensesService } from '../../services/expenses.service';
+import { Expense } from '@interfaces/expense';
 
 @Component({
   selector: 'monthly-stats',
@@ -13,13 +14,15 @@ export class MonthlyStatsComponent implements OnInit, OnDestroy {
 
   monthlyBudget = 4000.00;
   monthExpenses = 0;
+  highestExpense: {name: string, amount: number} | null = null;
 
   get runningTotal() {
     return this.monthlyBudget - this.monthExpenses;
   }
 
   ngOnInit(): void {
-    this.subscription = this.expensesService.listOfExpenses$.subscribe((_) => {
+    this.subscription = this.expensesService.listOfExpenses$.subscribe((expenses) => {
+      this.highestExpense = this.calculateHighestExpense(expenses);
       this.monthExpenses = this.expensesService.expensesTotalAmount;
     });
   }
@@ -28,6 +31,15 @@ export class MonthlyStatsComponent implements OnInit, OnDestroy {
     if(this.subscription) {
       this.subscription.unsubscribe();
     }
+  }
+
+  calculateHighestExpense(expenses: Expense[]) {
+    if (expenses.length === 0) return null;
+
+    const amounts = expenses.map(e => e.amount);
+    const categoryHighestAmount = Math.max(...amounts);
+    const category = expenses.find(e => e.amount === categoryHighestAmount)!;
+    return {name: category.category.name, amount: categoryHighestAmount};
   }
 
   constructor(private expensesService: ExpensesService) {}
