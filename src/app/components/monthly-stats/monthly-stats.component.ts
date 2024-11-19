737550dda7036a8417375806bf7e2d5b5ff9e469
mmoya26 <mmoya18@icloud.com>
2024-11-18 23:14:40 -0600

    create base components and css files for adjusting user budget

--- src/app/components/monthly-stats/monthly-stats.component.ts
+++ src/app/components/monthly-stats/monthly-stats.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ExpensesService } from '../../services/expenses.service';
 import { Expense } from '@interfaces/expense';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @Component({
   selector: 'monthly-stats',
   standalone: true,
-  imports: [],
+  imports: [InputNumberModule],
   templateUrl: './monthly-stats.component.html',
+  styleUrl: './monthly-stats.component.css'
 })
 export class MonthlyStatsComponent implements OnInit, OnDestroy {
   private subscription: Subscription = new Subscription();
 
+  @ViewChild('budgetInput') budgetInput!: ElementRef;
+
   monthlyBudget = 4000.00;
   monthExpenses = 0;
   highestExpense: {name: string, amount: number} | null = null;
+
+  editingBudget = false;
 
   get runningTotal() {
     return this.monthlyBudget - this.monthExpenses;
@@ -33,6 +39,10 @@ export class MonthlyStatsComponent implements OnInit, OnDestroy {
     }
   }
 
+  focusBudgetInput() {
+    this.budgetInput.nativeElement.focus();
+  }
+
   calculateHighestExpense(expenses: Expense[]) {
     if (expenses.length === 0) return null;
 
@@ -40,6 +50,14 @@ export class MonthlyStatsComponent implements OnInit, OnDestroy {
     const categoryHighestAmount = Math.max(...amounts);
     const category = expenses.find(e => e.amount === categoryHighestAmount)!;
     return {name: category.category.name, amount: categoryHighestAmount};
+  }
+
+  editBudget() {
+    this.editingBudget = true;
+  }
+
+  stopEditingBudget() {
+    // this.editingBudget = false;
   }
 
   constructor(private expensesService: ExpensesService) {}
